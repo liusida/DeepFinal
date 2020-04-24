019935d88089ff42b13cdee8cf7d1a4936e55f3e
sida <sliu1@uvm.edu>
2020-04-24 13:13:20 -0400

    insert dnn

--- 8.evolution.py
+++ 8.evolution.py
@@ -1,2 +1,187 @@
 # Evolution with and without DNN surrogate
 
+# This file contains high level logic.
+
+# psuedocode of this file
+#
+# if last generation of population exist
+#   read them in
+# else
+#   randomly generate a population
+# send the population to simulation
+# read the output report
+# select high fitness population
+# use report to train a model
+# use the model to mutate high fitness population
+# write the next generation of population with high fitnesses and mutants
+
+from exp_settings import *
+
+""" == Settings for Deep Learning == """
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import time, sys, math
+import numpy as np
+
+# my packages
+import preprocess
+import datasets
+import visualize
+import networks
+
+torch.manual_seed(seed)
+
+GPU = True
+if GPU:
+    dtype = torch.cuda.FloatTensor
+    device = 'GPU'
+else:
+    dtype = torch.FloatTensor
+    device = 'RAM'
+net = networks.FC4()
+if GPU:
+    net.cuda()
+training_epochs_per_generation = 10
+criterion = nn.MSELoss()
+optimizer = optim.SGD(net.parameters(), lr=0.01)
+batch_size = 512
+train_X = None
+train_Y = None
+# train_X.shape [?, 6, 6, 6, 5]
+# train_Y.shape [?, 1]
+
+def body_one_hot_gpu(body, num_classes=5):
+    """ body is a numpy [?,6,6,6] array, with max number of num_classes-1, say 4. """
+    body_t = torch.from_numpy(body).type(torch.cuda.LongTensor)
+    batch_size = body_t.size()[0]
+    body_t = body_t.view(batch_size,6,6,6,1)
+    # One hot encoding buffer that you create out of the loop and just keep reusing
+    body_onehot = torch.cuda.FloatTensor(batch_size, 6,6,6, num_classes).zero_()
+    body_onehot.scatter_(4, body_t, 1)
+    return body_onehot
+
+""" == Settings for Evolution == """
+import voxelyze as vx
+from voxelyze.evolution.cppn_alife.CPPNEvolution import CPPNEvolution
+import numpy as np
+import shutil, random, os
+DNN = True
+generation = 0
+
+
+vx.clear_workspace()
+
+# try to resume from last experiment
+evolution_dic, generation = vx.load_last_generation(experiment_name)
+# if failed, start from scratch
+if evolution_dic is None:
+    generation = 0
+    evolution = CPPNEvolution(body_dimension(), target_population_size(), mutation_rate())
+    evolution.init_geno(hidden_layers=hidden_layers)
+    evolution.express()
+else:
+    # resize using new body_dimension
+    evolution = CPPNEvolution()
+    init_body_dimension_n(evolution_dic["body_dimension"][0])
+    evolution.load_dic(evolution_dic)
+
+# infinity evolutionary loop
+while(True):
+    # write vxa vxd
+    foldername = vx.prepare_directories(experiment_name, generation)
+    vx.copy_vxa(experiment_name, generation)
+    vx.write_all_vxd(experiment_name, generation, evolution.dump_dic())
+
+    # before simulation, do a prediction
+    if DNN:
+        current_population_size = len(evolution.population['phenotype'])
+        current_X = []
+        for i in range(current_population_size):
+            current_X.append( evolution.population['phenotype'][i]['body'] )
+        current_X = np.array(current_X)
+        current_X_t = body_one_hot_gpu(current_X)
+        Y_hat = net(current_X_t)
+        sorted_id = torch.argsort(Y_hat, dim=0, descending=True).cpu().numpy().reshape(-1)
+        print(f"Predicted sort: {sorted_id}")
+
+    # start simulator
+    print("simulating...")
+    vx.start_simulator(experiment_name, generation)
+    # read report
+    sorted_result = vx.read_report(experiment_name, generation)
+    print(f"Result from simulation: {sorted_result['id']}")
+    # Insert DNN and train
+    if DNN:
+        current_population_size = len(evolution.population['phenotype'])
+        current_X = []
+        current_Y = []
+        for i in range(current_population_size):
+            robot_id = sorted_result['id'][i]
+            current_X.append( evolution.population['phenotype'][robot_id]['body'] )
+            current_Y.append( sorted_result['fitness'][i])
+        current_X = np.array(current_X)
+        current_Y = np.array(current_Y).reshape([-1,1])
+        current_X_t = body_one_hot_gpu(current_X)
+        current_Y_t = torch.from_numpy(current_Y).type(dtype)
+        # add to training set
+        if train_X is None:
+            train_X = current_X_t
+            train_Y = current_Y_t
+        else:
+            train_X = torch.cat([train_X, current_X_t])
+            train_Y = torch.cat([train_Y, current_Y_t])
+
+        # train
+        total_number = train_X.size()[0]
+        if total_number>10:
+            for epoch in range(training_epochs_per_generation):
+                for i in range(math.ceil(total_number/batch_size)):
+                    batch_start = i*batch_size
+                    batch_end = (i+1)*batch_size if (i+1)*batch_size<total_number-1 else total_number-1
+                    # print(f"{batch_start} - {batch_end}")
+                    train_X_batch = train_X[batch_start:batch_end]
+                    train_Y_batch = train_Y[batch_start:batch_end]
+
+                    optimizer.zero_grad()   # zero the gradient buffers
+                    Y_hat = net(train_X_batch)
+                    train_loss = criterion(Y_hat, train_Y_batch)
+                    train_loss.backward()
+                    optimizer.step()    # Does the update
+            Y_hat = net(current_X_t)
+            test_loss = criterion(Y_hat, current_Y_t)
+            msg = f"epoch: {epoch:05}; train_loss: {train_loss:.5f}; test_loss: {test_loss:.5f}. "
+            print(msg)
+            
+
+    # report the fitness
+    top_n = 3 #len(sorted_result['id'])
+    msg = f"Experiment {experiment_name}, simulation for generation {generation} finished.\nThe top {top_n} bestfit fitness score of this generation are \n"
+    for i in range(top_n):
+        if i<len(sorted_result['id']):
+            robot_id = sorted_result['id'][i]
+            msg += f"{evolution.population['genotype'][robot_id]['firstname']} {evolution.population['genotype'][robot_id]['lastname']}'s fitness score: {sorted_result['fitness'][i]:.1e} \n"
+    print(msg, flush=True)
+
+    # record a brief history for the bestfit
+    print("recording...")
+    vx.record_bestfit_history(experiment_name, generation, robot_id=sorted_result["id"][0], stopsec=2)
+
+    # vx.write_box_plot(experiment_name, generation, sorted_result)
+
+    # reporting
+    # import sida.slackbot.bot as bot
+    # bot.send(msg, 1, "GUB0XS56E")
+
+    # dynamical sceduling
+    evolution.target_population_size = target_population_size(generation)
+    evolution.body_dimension = body_dimension(generation, sorted_result["fitness"])
+    evolution.mutation_rate = mutation_rate(generation)
+
+    # write report png
+    # os.system("python plot_reports.py > /dev/null")
+    # next generation
+    generation += 1
+    next_generation = evolution.next_generation(sorted_result)
---
+++ exp_settings.py
@@ -0,0 +1,52 @@
+"""
+strategy: 
+search in 6x resolution, so we can quickly get a good base morphology (limbs).
+then upsampling to 30x, and evolve from there.
+"""
+
+import random
+import numpy as np
+from voxelyze.helper import cprint
+
+seed = 1000
+random.seed(seed)
+np.random.seed(seed)
+
+# ============== should be able to change during evolution: ================
+#  Plan: evolve a 100x100x100 body
+#  target generation = 500
+# 1. effect at this generation:
+# only if fitness score pass new height multiple times, increase body dimension
+# otherwise, keep adapting.
+
+best_last_round = 0
+body_dimension_n = 6
+fitness_score_surpass_time = 0
+
+def init_body_dimension_n(n):
+    global body_dimension_n
+    body_dimension_n = n
+
+def body_dimension(generation=0, fitness_scores=[0]):
+    # if generation<10:
+    return [6,6,6]
+    # else:
+        # return [8,8,8]
+        # return [30,30,30]
+
+def mutation_rate(generation=0):
+    # 19 times weight change, 1 time activation change
+    ret = [19, 0.1]
+    return ret
+
+def target_population_size(generation=0):
+    if generation<10:
+        return 40
+        # return 500
+    else:
+        return 40
+        # return 40
+
+# =================== cannot change during evolution: =======================
+experiment_name = f"Surrogate_wo_DNN_{seed}"
+hidden_layers = [10,10,10]
