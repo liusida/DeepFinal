13ae5c6d4cd5507dc94e520c7c3685032b20911d
sida <sliu1@uvm.edu>
2020-04-24 21:47:35 -0400

    better plot

--- plot_reports.py
+++ plot_reports.py
@@ -16,14 +16,21 @@ def target_population_size(generation=0):
     return 240
 experiment_name = sys.argv[1]
 # experiment_name = "v040711"
+max_gen = 101
+if len(sys.argv)>2:
+    max_gen = int(sys.argv[2])
 
+control_exp=False
+if "wo_" in experiment_name:
+    control_exp = True    
 x = []
 x1 = []
 x2 = []
 body = []
 mut = []
 pop = []
-for generation in range(10000):
+bestsofar = []
+for generation in range(max_gen):
     folder = vx.foldername_generation(experiment_name, generation)
     if folder is None:
         break
@@ -44,7 +51,7 @@ for generation in range(10000):
         end_x = float(robot.xpath("currentCenterOfMass/x")[0].text)
         end_y = float(robot.xpath("currentCenterOfMass/y")[0].text)
         end_z = float(robot.xpath("currentCenterOfMass/z")[0].text)
-        distance = np.sqrt((end_x-init_x)**2 + (end_y-init_y)**2)
+        distance = np.sqrt((end_x-init_x)**2 + (end_y-init_y)**2) / 0.1 / 1 # body length=0.1m, evaluate time=1sec
         num_voxel = int(robot.xpath("num_voxel")[0].text)
         distances.append(distance)
         end_zs.append(end_z)
@@ -55,39 +62,33 @@ for generation in range(10000):
     x.append(distances)
     x1.append(num_voxels)
     x2.append(end_zs)
+    best_this_gen = np.max(x)
+    bestsofar.append(best_this_gen)
 
 large_f = max(x[-1])
 ticks = []
-stepsize = int(len(x)/10)+1
+stepsize = 10
 for i in range(len(x)):
     if i%stepsize==0:
         ticks.append(i)
-flierprops = dict(marker='o', markerfacecolor='gray', markersize=3, linestyle='none')
-plt.boxplot(x, flierprops=flierprops)
+flierprops = dict(marker='o', markerfacecolor='#999999', markersize=2, linestyle='none', markeredgecolor="#eeeeee")
+whiskerprops = dict(color="#999999")
+boxprops = dict(color="#999999")
+plt.figure(figsize=[6,6])
+plt.boxplot(x, positions=list(range(len(x))), flierprops=flierprops, whiskerprops=whiskerprops, boxprops=boxprops)
 plt.xticks(ticks, ticks)
-
-large_b = body[-1]
-body = np.array(body)
-body = body * large_f / large_b
-
-large_m = mut[0]
-mut = np.array(mut)
-mut = mut * large_f / large_m
-
-large_p = pop[-1]
-pop = np.array(pop)
-pop = pop * large_f / large_p
-
-xx = list(range(len(x)))
-print(len(xx), len(body))
-plt.plot(xx,body, label=f"body: {large_b}")
-plt.plot(xx,mut, label=f"mutate: {large_m}")
-plt.plot(xx,pop, label=f"pop: {large_p}")
-plt.legend()
-plt.ylabel("Travel Distance")
+plt.plot(list(range(len(x))), bestsofar, label="Best so far")
+plt.legend(loc='upper left')
+if not control_exp:
+    plt.annotate('Start using DNN', xy=(41, 0), xytext=(45, 0.1),
+            arrowprops=dict(facecolor='black', shrink=0.05, width=2, headwidth=5),
+            )
+plt.ylabel("Travel Speed (body lengths/s)")
 plt.xlabel("Generation")
+plt.tight_layout()
 plt.savefig(f"boxplot_{experiment_name}.svg")
 plt.close()
+
 
 plt.figure(figsize=(9,6))
 pboxplot = plt.boxplot(x1, showfliers=False)
